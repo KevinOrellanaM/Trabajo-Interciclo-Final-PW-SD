e68fcde99bf4a0b43b175aa5a6a9898c9de3b94d
Kevin Orellana <kevin.orellana01@ucuenca.edu.ec>
2025-11-14 07:59:26 -0500

    KO - UPDATE - updated route LiveCameras and delete unused Photos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,6 @@ import Layout from "./components/Layout.jsx";
 import Home from "./pages/Home.jsx";
 import News from "./pages/News.jsx";
 import LiveCameras from "./pages/LiveCameras.jsx";
-import Photos from "./pages/Photos.jsx";
 import Contact from "./pages/Contact.jsx";
 
 export default function App() {
