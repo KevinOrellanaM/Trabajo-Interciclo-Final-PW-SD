4987d02da560fad92ffdf82a98aec46d4b1520a3
Andrea Maza <andrea.mazap@ucuenca.edu.ec>
2025-11-14 01:51:14 -0500

    cambiado a 'tendencias-service'

--- frontend/src/pages/News.jsx
+++ frontend/src/pages/News.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { getTendencias } from "../services/tendenciasService";
+import { getTendencias } from "../services/tendencias-service";
 
 export default function News() {
   const [tendencias, setTendencias] = useState([]);
